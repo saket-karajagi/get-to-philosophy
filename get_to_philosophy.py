4b29c267443e5b7ad2c27cab31833e38c22ea574
skrgi <saket.karajagi@pager.com>
2021-03-20 20:44:26 -0400

    fix: comments

--- get_to_philosophy.py
+++ get_to_philosophy.py
@@ -19,11 +19,13 @@ class WikipediaTrace:
         soup = BeautifulSoup(response_text, features='lxml')
         main_text = soup.find(
             id='mw-content-text').find(class_='mw-parser-output')
-
+        
+        # only fetch links in the main text (ignores italicized paragraphs)
         for element in main_text.find_all("p", recursive=False):
             d = element.contents
             count = 0
             for ele in range(0, len(d)):
+                # avoid links that are contained in paranthesis
                 if '(' in d[ele]:
                     count += 1
                 elif ')' in d[ele] and count > 0:
@@ -34,7 +36,7 @@ class WikipediaTrace:
                 else:
                     continue
 
-            if not next_first_page:
+            if not next_first_page: # reached a dead end
                 continue
             else:
                 break
@@ -49,45 +51,42 @@ class WikipediaTrace:
 
     def trace_types(self):
         for _ in range(0, number_of_random_links):
-            first_url = 'https://en.wikipedia.org/wiki/Special:Random'
+            first_url = 'https://en.wikipedia.org/wiki/Special:Random' # picks a random article
             last_url = 'https://en.wikipedia.org/wiki/Philosophy'
 
             reached = False
-            wiki_tree = [first_url]
             wiki_seen = set()
             step_count = 0
 
             while not reached:
                 next_first_url = self.get_next_first_url(wiki_tree[-1])
-                print(next_first_url)
                 step_count += 1
                 if not next_first_url:
                     self.last_seen[next_first_url] = self.last_seen.get(next_first_url, 0) + 1
                     types['reached dead end'] += 1
                     reached = True
-                elif next_first_url == last_url:
+                elif next_first_url == last_url: # reached Philosophy
                     types['target reached'] += 1
-                    if step_count > self.max_path:
+                    if step_count > self.max_path: # update the chain count if greater than max
                     	self.max_path = step_count
                     reached = True
-                elif step_count >= 100:
+                elif step_count >= 100: # have we reached the max limit for the chain?
                     self.last_seen[next_first_url] = self.last_seen.get(next_first_url, 0) + 1
                     types['max steps reached'] += 1
                     reached = True
-                elif next_first_url in wiki_seen:
+                elif next_first_url in wiki_seen: # is there a loop in the chain?
                     self.last_seen[next_first_url] = self.last_seen.get(next_first_url, 0) + 1
                     types['loop'] += 1
                     reached = True
                 else:
-                    wiki_tree.append(next_first_url)
-                    wiki_seen.add(next_first_url)
+                    wiki_seen.add(next_first_url) # add to the chain
 
         return types, self.last_seen, self.max_path
 
 types = {'reached dead end': 0, 
          'target reached': 0, 
          'max steps reached': 0, 
-         'loop': 0,}
-number_of_random_links = 100
+         'loop': 0,} #initialize all types
+number_of_random_links = 100 # sample set
 wiki_trace = WikipediaTrace(types, number_of_random_links)
 print(wiki_trace.trace_types())
